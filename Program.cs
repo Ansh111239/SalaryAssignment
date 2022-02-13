6319c1d4e615fea8226025300de1d1c5091518fe
Ansh111239 <99594251+Ansh111239@users.noreply.github.com>
2022-02-13 18:10:56 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Porj1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your Salary: ");
+            int salary = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the number of Shifts: ");
+            int shift = Convert.ToInt32(Console.ReadLine());
+            int saving;
+
+
+            if (salary < 0)
+            {
+                Console.WriteLine("Salary too small");
+            }
+            if (salary > 8000)
+            {
+                Console.WriteLine("Salary too large");
+            }
+            if (shift < 0)
+            {
+                Console.WriteLine("Shift too small");
+            }
+            else
+            {
+                saving = (int)(salary - ((30 / 100.00) * salary) - ((20 / 100) * salary) + ((2 / 100) * salary * shift));
+                Console.WriteLine(saving);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
